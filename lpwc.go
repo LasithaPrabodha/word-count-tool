67bd926bcf10a65dc6ab51da96de081f92cc4346
Lasitha Weligampola <lasiprabo@gmail.com>
2024-02-23 20:07:22 -0500

    word count

--- lpwc.go
+++ lpwc.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) != 3 || (os.Args[1] != "-c" && os.Args[1] != "-l") {
-		fmt.Println("Usage:\tlpwc\t-c <file_name>\n\t\t-l <file_name>")
+	if len(os.Args) != 3 || (os.Args[1] != "-c" && os.Args[1] != "-l" && os.Args[1] != "-w") {
+		fmt.Println("Usage:\tlpwc\t-c <file_name>\n\t\t-l <file_name>\n\t\t-w <file_name>")
 		os.Exit(1)
 	}
 
@@ -19,13 +20,14 @@ func main() {
 
 	if err != nil {
 		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+
 	var count int
 	if option == "-l" {
-		scanner := bufio.NewScanner(file)
-
 		for scanner.Scan() {
 			count++
 		}
@@ -38,6 +40,13 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("%10d %s\n", fileInfo.Size(), fileName)
+	} else if option == "-w" {
+		var wordCount int
+		for scanner.Scan() {
+			words := strings.Fields(scanner.Text())
+			wordCount += len(words)
+		}
+		fmt.Printf("%10d %s\n", wordCount, fileName)
 	}
 
 }
